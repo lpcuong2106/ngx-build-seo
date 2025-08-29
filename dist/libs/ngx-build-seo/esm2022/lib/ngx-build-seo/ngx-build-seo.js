dd156fff364cf9f23122a24e71dd070db937dc92
lpcuong2106 <lpcuong2106@gmail.com>
2025-08-29 22:22:19 +0700

    feat: add gitaction

--- dist/libs/ngx-build-seo/esm2022/lib/ngx-build-seo/ngx-build-seo.js
+++ dist/libs/ngx-build-seo/esm2022/lib/ngx-build-seo/ngx-build-seo.js
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
 import * as i0 from "@angular/core";
 export class NgxBuildSeo {
+    constructor() {
+    }
+    test() {
+        console.log('ngx-build-seo');
+    }
     static ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "20.1.8", ngImport: i0, type: NgxBuildSeo, deps: [], target: i0.ɵɵFactoryTarget.Component });
-    static ɵcmp = i0.ɵɵngDeclareComponent({ minVersion: "14.0.0", version: "20.1.8", type: NgxBuildSeo, isStandalone: true, selector: "lib-ngx-build-seo", ngImport: i0, template: "<p>NgxBuildSeo works!</p>\n", styles: [""] });
+    static ɵcmp = i0.ɵɵngDeclareComponent({ minVersion: "14.0.0", version: "20.1.8", type: NgxBuildSeo, isStandalone: true, selector: "lib-ngx-build-seo", ngImport: i0, template: "<p>NgxBuildSeo works!1</p>\n", styles: [""] });
 }
 i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "20.1.8", ngImport: i0, type: NgxBuildSeo, decorators: [{
             type: Component,
-            args: [{ selector: 'lib-ngx-build-seo', imports: [], template: "<p>NgxBuildSeo works!</p>\n" }]
-        }] });
+            args: [{ selector: 'lib-ngx-build-seo', imports: [], template: "<p>NgxBuildSeo works!1</p>\n" }]
+        }], ctorParameters: () => [] });
 //# sourceMappingURL=ngx-build-seo.js.map
